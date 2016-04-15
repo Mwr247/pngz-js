9ef1fc8776e2620e221165a23bdf309f273eadc2
Michael Rogers <mwr247@gmail.com>
2016-04-15 03:27:12 -0400

    First version, all working

--- index.js
+++ index.js
@@ -2,42 +2,57 @@
 var zlib = require('zlib');
 var crypto = require('crypto');
 var PNG = require('pngjs').PNG;
-var fs = require('fs');
 
 // Config
 var algorithm = 'aes-256-cbc';
 var ivLength = 16;
+var keyLength = 32;
 var hashAlgorithm = 'sha256';
+
+// Output PNG
+function toPNG(buffer, callback) { // Buffer, Function
+  var dim = Math.sqrt(buffer.length / 3);
+  var png = new PNG({
+    width: Math.ceil(dim),
+    height: Math.round(dim),
+    colorType: 2,
+    inputHasAlpha: false
+  });
+  var pixels = [];
+  png.data = buffer;
+  png.pack().on('data', function(data) {
+    pixels.push(data);
+  }).on('end', function() {
+    callback(null, Buffer.concat(pixels));
+  });
+}
 
 // Takes raw data and returns a PNG image buffer
 function encode(data, password, callback) { // Buffer[, String], Function
   if (typeof password === 'function') {
     callback = password;
-    password = '';
+    password = null;
   }
   zlib.deflateRaw(data, function(err, buffer) {
-    if (err) {throw new Error(err);}
-    var key = crypto.createHash(hashAlgorithm).update(password).digest();
-    var iv = crypto.randomBytes(ivLength);
-    var cipher = crypto.createCipheriv(algorithm, key, iv);
-    var encrypted = Buffer.concat([iv, cipher.update(buffer), cipher.final()]);
-    zlib.deflateRaw(encrypted, function(err, buffer) {
-      if (err) {throw new Error(err);}
-      var dim = Math.sqrt(buffer.length / 3);
-      var png = new PNG({
-        width: Math.ceil(dim),
-        height: Math.round(dim),
-        colorType: 2,
-        inputHasAlpha: false
-      });
-      var pixels = [];
-      png.data = buffer;
-      png.pack().on('data', function(data) {
-        pixels.push(data);
-      }).on('end', function() {
-        callback(Buffer.concat(pixels));
-      });
-    });
+    if (err) {
+      callback(new Error("Could not deflate"), null);
+    } else {
+      if (password != null && password.length > 0) {
+        var iv = crypto.randomBytes(ivLength);
+        var key = crypto.createHash(hashAlgorithm).update(Buffer.concat([new Buffer(password), iv])).digest().slice(0, keyLength);
+        var cipher = crypto.createCipheriv(algorithm, key, iv);
+        var encrypted = Buffer.concat([iv, cipher.update(buffer), cipher.final()]);
+        zlib.deflateRaw(encrypted, function(err, buffer) {
+          if (err) {
+            callback(new Error("Could not deflate"), null);
+          } else {
+            toPNG(buffer, callback);
+          }
+        });
+      } else {
+        toPNG(buffer, callback);
+      }
+    }
   });
 }
 
@@ -45,40 +60,48 @@ function encode(data, password, callback) { // Buffer[, String], Function
 function decode(png, password, callback) { // Buffer[, String], Function
   if (typeof password === 'function') {
     callback = password;
-    password = '';
+    password = null;
   }
   new PNG().parse(png, function(err, buffer){
-    if (err) {throw new Error(err);}
-    var pixels = [];
-    var len = buffer.data.length;
-    for (var i = 0; i < len; i = i + 4) {
-      pixels.push(buffer.data.slice(i, i + 3));
-    }
-    zlib.inflateRaw(Buffer.concat(pixels), function(err, buffer) {
-      if (err) {throw new Error(err);}
-      var key = crypto.createHash(hashAlgorithm).update(password).digest();
-      var iv = buffer.slice(0, ivLength);
-      var decipher = crypto.createDecipheriv(algorithm, key, iv);
-      var decrypted = Buffer.concat([decipher.update(buffer.slice(ivLength)), decipher.final()]);
-      zlib.inflateRaw(decrypted, function(err, data) {
-        if (err) {throw new Error(err);}
-        callback(data);
+    if (err) {
+      callback(new Error("Not a PNG image"), null);
+    } else {
+      var pixels = [];
+      var len = buffer.data.length;
+      for (var i = 0; i < len; i = i + 4) {
+        pixels.push(buffer.data.slice(i, i + 3));
+      }
+      zlib.inflateRaw(Buffer.concat(pixels), function(err, buffer) {
+        if (err) {
+          callback(new Error("Not a PNGz image"), null);
+        } else {
+          if (password != null && password.length > 0) {
+            var iv = buffer.slice(0, ivLength);
+            var key = crypto.createHash(hashAlgorithm).update(Buffer.concat([new Buffer(password), iv])).digest().slice(0, keyLength);
+            var decipher = crypto.createDecipheriv(algorithm, key, iv);
+            try {
+              var decrypted = Buffer.concat([decipher.update(buffer.slice(ivLength)), decipher.final()]);
+            } catch(err) {
+              callback(new Error("Invalid password"), null);
+              return;
+            }
+            zlib.inflateRaw(decrypted, function(err, data) {
+              if (err) {
+                callback(new Error("Not a PNGz image"), null);
+              } else {
+                callback(null, data);
+              }
+            });
+          } else {
+            callback(null, buffer);
+          }
+        }
       });
-    });
+    }
   });
 }
 
-var test = fs.readFileSync('index.js', 'ascii');
-//console.log(test);
-encode(test, '', function(data) {
-  //console.log(data);
-  fs.writeFileSync('test.png', data);
-  decode(data, '', function(data2) {
-    //console.log(data2);
-    fs.writeFileSync('test.txt', data2);
-  });
-});
-
+// Export
 module.exports = {
   encode: encode,
   decode: decode
